6655f119eca23597e4aa9063c4f39ce6d2c5dcea
Blair Haukedal <blhauk@gmail.com>
2020-03-03 16:17:53 -0700

    Add Humidity, rearrange output in ForecastViewContoller

--- MyWeather/Controller/ForecastViewContoller.swift
+++ MyWeather/Controller/ForecastViewContoller.swift
@@ -15,103 +15,99 @@ class ForecastViewContoller: UIViewController {
     
     @IBOutlet weak var cityName: UILabel!
     
-    @IBOutlet weak var day1DateLow: UILabel!
-    @IBOutlet weak var day1DateHigh: UILabel!
-    @IBOutlet weak var day2DateLow: UILabel!
-    @IBOutlet weak var day2DateHigh: UILabel!
-    @IBOutlet weak var day3DateLow: UILabel!
-    @IBOutlet weak var day3DateHigh: UILabel!
-    @IBOutlet weak var day4DateLow: UILabel!
-    @IBOutlet weak var day4DateHigh: UILabel!
-    @IBOutlet weak var day5DateLow: UILabel!
-    @IBOutlet weak var day5DateHigh: UILabel!
-    @IBOutlet weak var day6DateLow: UILabel!
-    @IBOutlet weak var day6DateHigh: UILabel!
-    @IBOutlet weak var day7DateLow: UILabel!
-    @IBOutlet weak var day7DateHigh: UILabel!
+    @IBOutlet weak var day1Date: UILabel!
+    @IBOutlet weak var day2Date: UILabel!
+    @IBOutlet weak var day3Date: UILabel!
+    @IBOutlet weak var day4Date: UILabel!
+    @IBOutlet weak var day5Date: UILabel!
+    @IBOutlet weak var day6Date: UILabel!
+    @IBOutlet weak var day7Date: UILabel!
     
     @IBOutlet weak var day1Low: UILabel!
     @IBOutlet weak var day1High: UILabel!
+    @IBOutlet weak var day1Hum: UILabel!
+    
     @IBOutlet weak var day2Low: UILabel!
     @IBOutlet weak var day2High: UILabel!
+    @IBOutlet weak var day2Hum: UILabel!
+    
     @IBOutlet weak var day3Low: UILabel!
     @IBOutlet weak var day3High: UILabel!
+    @IBOutlet weak var day3Hum: UILabel!
+    
     @IBOutlet weak var day4Low: UILabel!
     @IBOutlet weak var day4High: UILabel!
+    @IBOutlet weak var day4Hum: UILabel!
+    
     @IBOutlet weak var day5Low: UILabel!
     @IBOutlet weak var day5High: UILabel!
+    @IBOutlet weak var day5Hum: UILabel!
+    
     @IBOutlet weak var day6Low: UILabel!
     @IBOutlet weak var day6High: UILabel!
+    @IBOutlet weak var day6Hum: UILabel!
+    
     @IBOutlet weak var day7Low: UILabel!
     @IBOutlet weak var day7High: UILabel!
+    @IBOutlet weak var day7Hum: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        var date: String
-        var lowTemp: String
-        var highTemp: String
+        var dates:      [String] = []
+        var lowTemps:   [String] = []
+        var highTemps:  [String] = []
+        var humidities: [String] = []
+
         let city = sharedData.providedLocation
         let countryCode = sharedData.countryCode
         let countryName = countryCodes[countryCode]
         cityName.text = city + ", " + (countryName ?? "Unkown country")
         
-        date = getLocalDate(epochTime: sharedData.forecastTime[0], offset: sharedData.forecastOffset[0])
-        lowTemp =  String(format: "%3.1f°C", sharedData.forecastLowTemp[0])
-        highTemp = String(format: "%3.1f°C", sharedData.forecastHighTemp[0])
-        day1DateLow.text = date + " Low:"
-        day1Low.text = lowTemp
-        day1DateHigh.text = date + " High:"
-        day1High.text = highTemp
+        var date: String
+        for i in 0...6 {
+            date = getLocalDate(epochTime: sharedData.forecastTime[i], offset: sharedData.forecastOffset[i])
+            dates.append(date)
+            lowTemps.append(String(format: "%3.1f°C", sharedData.forecastLowTemp[i]))
+            highTemps.append(String(format: "%3.1f°C", sharedData.forecastHighTemp[i]))
+            humidities.append(String(format: "%3.0f%%", sharedData.forecastHumidity[i]*100))
+        }
 
-        date = getLocalDate(epochTime: sharedData.forecastTime[1], offset: sharedData.forecastOffset[1])
-        lowTemp =  String(format: "%3.1f°C", sharedData.forecastLowTemp[1])
-        highTemp = String(format: "%3.1f°C", sharedData.forecastHighTemp[1])
-        day2DateLow.text = date + " Low:"
-        day2Low.text = lowTemp
-        day2DateHigh.text = date + " High:"
-        day2High.text = highTemp
+        day1Date.text = dates[0]
+        day1Low.text =  lowTemps[0]
+        day1High.text = highTemps[0]
+        day1Hum.text =  humidities[0]
 
-        date = getLocalDate(epochTime: sharedData.forecastTime[2], offset: sharedData.forecastOffset[2])
-        lowTemp =  String(format: "%3.1f°C", sharedData.forecastLowTemp[2])
-        highTemp = String(format: "%3.1f°C", sharedData.forecastHighTemp[2])
-        day3DateLow.text = date + " Low:"
-        day3Low.text = lowTemp
-        day3DateHigh.text = date + " High:"
-        day3High.text = highTemp
+        day2Date.text = dates[1]
+        day2Low.text =  lowTemps[1]
+        day2High.text = highTemps[1]
+        day2Hum.text =  humidities[1]
 
-        date = getLocalDate(epochTime: sharedData.forecastTime[3], offset: sharedData.forecastOffset[3])
-        lowTemp =  String(format: "%3.1f°C", sharedData.forecastLowTemp[3])
-        highTemp = String(format: "%3.1f°C", sharedData.forecastHighTemp[3])
-        day4DateLow.text = date + " Low:"
-        day4Low.text = lowTemp
-        day4DateHigh.text = date + " High:"
-        day4High.text = highTemp
+        day3Date.text = dates[2]
+        day3Low.text =  lowTemps[2]
+        day3High.text = highTemps[2]
+        day3Hum.text =  humidities[2]
 
-        date = getLocalDate(epochTime: sharedData.forecastTime[4], offset: sharedData.forecastOffset[4])
-        lowTemp =  String(format: "%3.1f°C", sharedData.forecastLowTemp[4])
-        highTemp = String(format: "%3.1f°C", sharedData.forecastHighTemp[4])
-        day5DateLow.text = date + " Low:"
-        day5Low.text = lowTemp
-        day5DateHigh.text = date + " High:"
-        day5High.text = highTemp
+        day4Date.text = dates[3]
+        day4Low.text =  lowTemps[3]
+        day4High.text = highTemps[3]
+        day4Hum.text =  humidities[3]
 
-        date = getLocalDate(epochTime: sharedData.forecastTime[5], offset: sharedData.forecastOffset[5])
-        lowTemp =  String(format: "%3.1f°C", sharedData.forecastLowTemp[5])
-        highTemp = String(format: "%3.1f°C", sharedData.forecastHighTemp[5])
-        day6DateLow.text = date + " Low:"
-        day6Low.text = lowTemp
-        day6DateHigh.text = date + " High:"
-        day6High.text = highTemp
+        day5Date.text = dates[4]
+        day5Low.text =  lowTemps[4]
+        day5High.text = highTemps[4]
+        day5Hum.text =  humidities[4]
 
-        date = getLocalDate(epochTime: sharedData.forecastTime[6], offset: sharedData.forecastOffset[6])
-        lowTemp =  String(format: "%3.1f°C", sharedData.forecastLowTemp[6])
-        highTemp = String(format: "%3.1f°C", sharedData.forecastHighTemp[6])
-        day7DateLow.text = date + " Low:"
-        day7Low.text = lowTemp
-        day7DateHigh.text = date + " High:"
-        day7High.text = highTemp
+        day6Date.text = dates[5]
+        day6Low.text =  lowTemps[5]
+        day6High.text = highTemps[5]
+        day6Hum.text =  humidities[5]
+
+        day7Date.text = dates[6]
+        day7Low.text =  lowTemps[6]
+        day7High.text = highTemps[6]
+        day7Hum.text =  humidities[6]
 
 
     }
